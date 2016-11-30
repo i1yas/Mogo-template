8a0e8e993cced5c547a2f9d75086abf1a9cf907f
i1yas <ilyasers@gmail.com>
2016-11-30 12:14:57 +0300

    Navigation

---
+++ app/blocks/header/scroll.js
@@ -0,0 +1,18 @@
+const scroll = $ => {
+	$(() => {
+		$('a[href*="#"]:not([href="#"])').click(function () {
+			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+				let target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+};
+
+module.exports = scroll;
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,7 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import 'jquery-mousewheel';
 import 'malihu-custom-scrollbar-plugin';
+import scroll from '../blocks/header/scroll';
 import search from '../blocks/header/search';
 import slider from '../blocks/slider/slider';
 import service from '../blocks/service/service';
@@ -10,6 +11,7 @@ import quotesSlider from '../blocks/quotes-slider/quotes-slider.js';
 
 $(() => {
 	svg4everybody();
+	scroll($);
 	search();
 	slider();
 	service($);
